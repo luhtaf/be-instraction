c883283df4c5bae40b140b022c26c22fe3853e7a
torche <agung.pramono@torcheindonesia.com>
2024-10-24 10:43:50 +0700

    fix migration user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->string('unit_kerja')->nullable();
             $table->string('guid_dws')->nullable();
             $table->string('role');
